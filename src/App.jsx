ad93544d2a10e4dfe85c10aa8fd62354c4c325d6
Camila Ferreira <pferreiracamila@gmail.com>
2026-01-22 23:07:49 +0100

    correction on file extension

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherSearch from "./WeatherSearch.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="app" className="container">
+    <div className="app container">
       <h1>Weather Search App</h1>
+      <WeatherSearch />
+      <footer>
+        This project is developed by Camila Ferreira and it is open source on
+        Netlify
+      </footer>
     </div>
   );
 }
---
+++ src/WeatherSearch.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WeatherSearch() {
+  return <div className="weather-search">New York</div>;
+}
