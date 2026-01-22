a7fb24bb67c7e7457b25ca8090a558df00b28259
Camila Ferreira <pferreiracamila@gmail.com>
2026-01-22 22:43:17 +0100

    create a react weather app and install bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app" className="container">
+      <h1>Weather Search App</h1>
+    </div>
+  );
+}
+
+export default App;
