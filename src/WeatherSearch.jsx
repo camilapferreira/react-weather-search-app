eac64c28e5d2ba87987a99d1c8f30dd566b75731
Camila Ferreira <pferreiracamila@gmail.com>
2026-01-23 16:54:19 +0100

    update of the css of WeatherSearch component

--- src/WeatherSearch.jsx
+++ src/WeatherSearch.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./WeatherSearch.css";
 
 export default function WeatherSearch() {
   return (
@@ -22,6 +23,85 @@ export default function WeatherSearch() {
             </div>
           </div>
         </form>
+        <div className="Weather-info">
+          <div className="row">
+            <div className="col-6">
+              <h1>New York</h1>
+              <ul>
+                <li>
+                  <span>Friday 13:18</span>, few clouds
+                </li>
+                <li>
+                  Humidity: <strong>50%</strong>, Wind: <strong>10 km/h</strong>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-8">
+              <div className="temperature-container d-flex justify-content-end">
+                <canvas width="52" height="52"></canvas>
+                <div>
+                  <strong>10</strong>
+                  <span className="units">
+                    <a href="/">°C</a> | <a href="/">°F</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="WeatherForecast row">
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">Fri</div>
+              <canvas width="38" height="38"></canvas>
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">14°</span>
+                <span class="forecast-temperature-min">11°</span>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">Sat</div>
+              <canvas width="38" height="38"></canvas>
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">15°</span>
+                <span class="forecast-temperature-min">11°</span>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">Sun</div>
+              <canvas width="38" height="38"></canvas>
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">14°</span>
+                <span class="forecast-temperature-min">10°</span>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">Mon</div>
+              <canvas width="38" height="38"></canvas>
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">15°</span>
+                <span class="forecast-temperature-min">10°</span>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="WeatherForecastPreview">
+              <div class="forecast-time">Tue</div>
+              <canvas width="38" height="38"></canvas>
+              <div class="forecast-temperature">
+                <span class="forecast-temperature-max">15°</span>
+                <span class="forecast-temperature-min">10°</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <footer>
         This project was developed by Camila Ferreira and it is open source on{" "}
