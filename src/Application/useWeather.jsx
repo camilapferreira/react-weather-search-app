8f9350fe632da50ff07d1c091d1e61873a5cad63
Camila Ferreira <pferreiracamila@gmail.com>
2026-01-29 13:57:13 +0100

    create the format date component

--- src/Application/useWeather.jsx
+++ src/Application/useWeather.jsx
@@ -1,14 +1,71 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
-
-const useWeather = () => {
-  return getTemperature;
+export const formatDate = (data = Date.now()) => {
+  const date = new Date(data);
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const dayOfWeek = date.getDay();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const dayOfWeekName = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return `${dayOfWeekName[dayOfWeek]} ${hours}:${minutes}`;
 };
 
-function getTemperature(city) {
-  const apiKey = "44b4d9f5e3a3baf490c33c5519ot4f0a";
-  const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+const useWeather = () => {
+  const [city, setCity] = useState("Paris");
+  const [WeatherData, setWeatherData] = useState({});
+  const weekdays = Array.from({ length: 5 }, (_, index) => index + 1);
 
-  return axios.get(apiUrl);
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (city.length > 0) {
+      getTemperature(city).then(showData);
+    }
+  }
+  useEffect(() => {
+    getTemperature(city).then(showData);
+  }, []);
+
+  function showData(response) {
+    console.log(Date.now());
+    setWeatherData({
+      temperature: Math.round(response.data.temperature.current),
+      condition: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      icon: response.data.condition.icon_url,
+      city: response.data.city,
+    });
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+  function getTemperature(city) {
+    const apiKey = "44b4d9f5e3a3baf490c33c5519ot4f0a";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+    return axios.get(apiUrl);
+  }
+  return {
+    city,
+    WeatherData,
+    weekdays,
+    handleSubmit,
+    handleChange,
+    showData,
+    formatDate,
+  };
+};
 
 export { useWeather };
--- src/WeatherSearch.jsx
+++ src/WeatherSearch.jsx
@@ -1,34 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherSearch.css";
 import { useWeather } from "./Application/useWeather";
 
 export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [WeatherData, setWeatherData] = useState({});
-  const hook = useWeather();
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    if (city.length > 0) {
-      hook(city).then(showData);
-    }
-  }
-
-  function showData(response) {
-    setWeatherData({
-      temperature: Math.round(response.data.temperature.current),
-      condition: response.data.condition.description,
-      humidity: response.data.temperature.humidity,
-      wind: response.data.wind.speed,
-      icon: response.data.condition.icon_url,
-      city: response.data.city,
-    });
-  }
-
-  function handleChange(event) {
-    setCity(event.target.value);
-  }
+  const {
+    city,
+    WeatherData,
+    weekdays,
+    handleSubmit,
+    handleChange,
+    formatDate,
+  } = useWeather();
 
   return (
     <div className="container">
@@ -41,7 +23,7 @@ export default function WeatherSearch() {
                 placeholder="Enter city name"
                 className="form-control search-input"
                 onChange={handleChange}
-                value={WeatherData.city}
+                value={city}
               ></input>
             </div>
             <div className="col-3 p-0">
@@ -59,7 +41,7 @@ export default function WeatherSearch() {
               <h1>{WeatherData.city}</h1>
               <ul>
                 <li>
-                  <span>Friday 13:18</span>, {WeatherData.condition}
+                  <span>{formatDate()}</span>, {WeatherData.condition}
                 </li>
                 <li>
                   Humidity: <strong>{WeatherData.humidity}%</strong>, Wind:{" "}
@@ -82,71 +64,21 @@ export default function WeatherSearch() {
         </div>
 
         <div className="WeatherForecast row">
-          <div className="col">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time">Fri</div>
-              <img
-                src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                alt="weather icon"
-              />
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max">14°</span>
-                <span className="forecast-temperature-min">11°</span>
+          {weekdays.map((day) => (
+            <div className="col">
+              <div className="WeatherForecastPreview">
+                <div className="forecast-time">Fri</div>
+                <img
+                  src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
+                  alt="weather icon"
+                />
+                <div className="forecast-temperature">
+                  <span className="forecast-temperature-max">14°</span>
+                  <span className="forecast-temperature-min">11°</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time">Sat</div>
-              <img
-                src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                alt="weather icon"
-              />
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max">15°</span>
-                <span className="forecast-temperature-min">11°</span>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time">Sun</div>
-              <img
-                src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                alt="weather icon"
-              />
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max">14°</span>
-                <span className="forecast-temperature-min">10°</span>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time">Mon</div>
-              <img
-                src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                alt="weather icon"
-              />
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max">15°</span>
-                <span className="forecast-temperature-min">10°</span>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="WeatherForecastPreview">
-              <div className="forecast-time">Tue</div>
-              <img
-                src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
-                alt="weather icon"
-              />
-              <div className="forecast-temperature">
-                <span className="forecast-temperature-max">15°</span>
-                <span className="forecast-temperature-min">10°</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <footer>
